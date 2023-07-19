92860d94c012f506b0b524826d969845269e26b5
Shokir <tursinbekovshokirxon@gmail.com>
2023-07-19 16:57:34 +0500

    The Chat

---
+++ source/repos/Chat!/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_
+{
+    public class Person
+    {
+        public Person(string Name) {
+        this.Name = Name;   
+        } 
+        public string? Name { get; set; }
+        public List<string> Messages { get; set; }
+    }
+}
---
+++ source/repos/Chat!/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Chat_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+    public delegate void Sending(string message);
+    public class SendMessage
+    {
+        public Sending Sender;
+        public SendMessage()
+        {
+            Sender += SendI;
+        }
+        public void SendI(string message)
+        {
+            Sender.Invoke(message);
+        }
+    }
+}
